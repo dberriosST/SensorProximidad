fe4a8c27d2c6289bbba5d9d729d523511455314e
dberrios <d.berrios6@alumnos.santotomas.cl>
2023-11-06 23:27:48 -0300

    SensorProximidad

---
+++ app/src/main/java/com/example/sensorproximidad/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.sensorproximidad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    LinearLayout ln;
+
+    SensorManager sm;
+
+    Sensor sensor;
+
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ln = findViewById(R.id.activity_main);
+        tv = findViewById(R.id.tv);
+        sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sm.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        sm.registerListener((SensorEventListener) this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+    @Override
+    public void onSensorChanged(SensorEvent event){
+        String text = String.valueOf(event.values[0]);
+        tv.setText(text);
+        float valor = Float.parseFloat(text);
+        if (valor == 0){
+            ln.setBackgroundColor(Color.GREEN);
+        } else {
+            ln.setBackgroundColor(Color.RED);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i){
+
+    }
+}
